ede65d9bd0094ed310c4e161f8c55effa1819875
technik-matrose <121881667+technik-matrose@users.noreply.github.com>
2023-01-16 23:05:57 +0100

    Update locustfile.py

--- flask-sklearn/locustfile.py
+++ flask-sklearn/locustfile.py
@@ -1,1 +1,34 @@
+from locust import HttpLocust, TaskSet, task
 
+class MyTaskSet(TaskSet):
+    @task
+    def get(self):
+        self.client.get("flask-ml-service.azurewebsites.net")
+        
+    @task
+    def post_predict(self):
+        self.client.post("flask-ml-service.azurewebsites.net/predict", json=
+        {  
+          "CHAS":{  
+          "0":0
+          },
+          "RM":{  
+          "0":6.575
+          },
+          "TAX":{  
+          "0":296.0
+          },
+          "PTRATIO":{  
+          "0":15.3
+          },
+          "B":{  
+          "0":396.9
+          },
+          "LSTAT":{  
+          "0":4.98
+          }
+        })
+                         
+         
+class MyLocust(HttpLocust):
+  task_set = MyTaskSet
